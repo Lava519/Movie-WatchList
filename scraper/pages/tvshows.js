2d1f4b6e6db7457f485c268d5ee1b94fe10a6390
Lava519 <lavaboi14@proton.me>
2026-01-15 23:30:30 +0100

    Fixed tvshows URL and img

--- scraper/pages/tvshows.js
+++ scraper/pages/tvshows.js
@@ -1,9 +1,9 @@
 const URL = 'https://www.imdb.com/chart/toptv/';
 const TVSHOWS = {
-    title: '.ipc-metadata-list > .ipc-metadata-list-summary-item > div > div > div > .ipc-title > .ipc-title-link-wrapper > h3',
-    image: '.ipc-metadata-list > .ipc-metadata-list-summary-item > div > .ipc-poster > .ipc-media > .ipc-image',
-    url: '.ipc-metadata-list > .ipc-metadata-list-summary-item > div > .ipc-poster > a',
-    rating: '.ipc-metadata-list > .ipc-metadata-list-summary-item > div > div > div > span > div > span'
+    title: 'div > div > div > .ipc-title > .ipc-title-link-wrapper > h3',
+    image: 'div > .ipc-poster > .ipc-media > .ipc-image',
+    url: 'div > div > div > .ipc-title > .ipc-title-link-wrapper',
+    rating: 'div > div > div > span > div > span > .ipc-rating-star--rating'
 };
 
 
@@ -14,7 +14,7 @@ const tvshows = async (page) => {
     await page.goto(URL);
     await page.waitForNetworkIdle();
     const tvTitles = await page.$$eval(TVSHOWS.title, el => el.map(e => e.textContent.slice(e.textContent.indexOf(' ') + 1)));
-    const tvImages = await page.$$eval(TVSHOWS.image, el => el.map(e => e.src));
+    const tvImages = await page.$$eval(TVSHOWS.image, el => el.map(e => e.src.slice(0, e.src.lastIndexOf('_V1'))+"jpg"));
     const tvURL = await page.$$eval(TVSHOWS.url, el => el.map(e => e.href.split('/')[e.href.split('/').length - 2]));
     const tvRating = await page.$$eval(TVSHOWS.rating, el => el.map(e => e.textContent.slice(0, 3)));
     for (i = 0; i < 100; ++i)
