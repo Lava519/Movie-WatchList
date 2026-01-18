3a1550df8656df8532adc308205e995fb9ca6d68
Lava519 <lavaboi14@proton.me>
2026-01-18 18:07:14 +0100

    Updated

--- scraper/controller.js
+++ scraper/controller.js
@@ -5,14 +5,23 @@ const { movies } = require('./pages/movies');
 const { tvshows } = require('./pages/tvshows');
 const { search } = require('./pages/search');
 
-async function writeJson(jsonData) {
-  await fs.writeFile('./cache/home.json', jsonData, err => {
+async function writeJson(path, jsonData) {
+  await fs.writeFile(path, jsonData, err => {
     if (err) {
       console.log('Error writing file', err)
-    } else {
-      console.log('Written to the file');
     }
   })
+}
+
+const readJson = async (path) => {
+  try {
+    const data = await fs.readFileSync(path, { encoding: 'utf8'});
+    if (data == "" || data == "{}")
+      return null;
+    return JSON.parse(data);
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 const startBrowser = async () => {
@@ -28,37 +37,58 @@ const startBrowser = async () => {
     return browser;
 }
 
-const scrape = async (browserInstance, data) => {
+const scrape = async (browserInstance, data, check = true) => {
     let browser;
-    try {
-        browser = await browserInstance;
-        let page = await browser.newPage();
-        await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36');
-        await page.setViewport({
-          width: 640,
-          height: 2560,
-          deviceScaleFactor: 1,
-        });
-        let returnData = {};
-        console.log(data);
+    let returnData = null;
+    if (check) {
+      try {
         switch (data) {
-            case "home":
-                returnData = home(page);
-                break;
-            case "movies":
-                returnData = movies(page);
-                break;
-            case "tv-shows":
-                returnData = tvshows(page);
-                break;
-            default:
-                returnData = search(page, data);
-                break;
-        }
+        case "home":
+        returnData = await readJson("./cache/home.json");
+        break;
+        case "movies":
+        returnData = await readJson("./cache/movies.json");
+        break;
+        case "tv-shows":
+        returnData = await readJson("./cache/tvshows.json");
+        break;
+      }
+    } catch (err) {
+      console.log("Error reading JSON", err);
+    }
+            if (returnData)
         return returnData;
+    }
+    try {
+      browser = await browserInstance;
+      let page = await browser.newPage();
+      await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36');
+      await page.setViewport({
+        width: 640,
+        height: 2560,
+        deviceScaleFactor: 1,
+      });
+      switch (data) {
+          case "home":
+              returnData = await home(page);
+              writeJson("./cache/home.json", JSON.stringify(returnData));
+              break;
+          case "movies":
+              returnData = await movies(page);
+              writeJson("./cache/movies.json", JSON.stringify(returnData));
+              break;
+          case "tv-shows":
+              returnData = await tvshows(page);
+              writeJson("./cache/tvshows.json", JSON.stringify(returnData));
+              break;
+          default:
+              returnData = await search(page, data);
+              break;
+        }
     } catch (err) {
         console.log("Could not resolve the browser instance => ", err);
     }
+    return returnData;
 }
 
 module.exports = {
