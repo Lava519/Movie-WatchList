b64f768eee96367aa2eae494cacc058662402d7d
Lava519 <lavaboi14@proton.me>
2026-01-20 00:43:55 +0100

    Updated

--- frontend/src/components/advancedSearch/AdvancedSearch.jsx
+++ frontend/src/components/advancedSearch/AdvancedSearch.jsx
@@ -1,8 +1,7 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import "./AdvancedSearch.css";
 
 function SearchSelectButtons() {
-  const [setTSearch, tSearch] = useState("movies");
   return (
     <div>
       
@@ -10,20 +9,28 @@ function SearchSelectButtons() {
   )
 }
 
-function SearchButton(image, isToggled, toggle) {
-
+function SearchButton({image, toggle, setToggle}) {
   return (
-    <div onClick={toggle}>
-      <img src={`${"../../../public/"}${image}${".svg"}`}></img>
+    <div className="abutton" >
+      <img onClick={()=>{setToggle(image)}} className={`${image == toggle && "active"}`} src={`${"../../../public/"}${image}${".svg"}`}></img>
     </div>
   )
 }
 
 export default function AdvancedSearch() {
+  const [tSearch, setTSearch] = useState("movies");
+  const setToggle = (name)=> {
+    setTSearch(name)
+  }
   return (
     <div className="asearch-container">
       <div className="asearch">
         <input className="search" type="text"/>
+        <div className="abuttons-container">
+          <SearchButton image="movies" toggle={tSearch} setToggle={setToggle}></SearchButton>
+          <SearchButton image="person" toggle={tSearch} setToggle={setToggle}></SearchButton>
+          <SearchButton image="tv-shows" toggle={tSearch} setToggle={setToggle}></SearchButton>
+        </div>
       </div>
     </div>
   )
